39bba1db033e5d7f7bf0ff053d8247d58c84f883
combattardigrade <combattardigrade@gmail.com>
2019-04-13 17:14:04 -0500

    order answered questions by timestamp

--- src/components/AnsweredQuestions.js
+++ src/components/AnsweredQuestions.js
@@ -5,34 +5,34 @@ import { Link } from 'react-router-dom'
 function AnsweredQuestions(props) {
     const { questions, users, authedUser } = props
     const { answers } = users[authedUser]
-    
+
     return (
         <div style={{ padding: 10, border: '1px solid #d8d8d8' }}>
             {
-                Object.keys(answers).sort((a,b) => b.timestamp - a.timestamp).map((a) => (
-                    <Card key={questions[a].id} className="mt-3">
+                Object.values(questions).filter((q) => Object.keys(answers).includes(q.id)).sort((a, b) => b.timestamp - a.timestamp).map((q) => (
+                    <Card key={q.id} className="mt-3">
                         <Card.Header>
-                            {users[questions[a].author].name} asks:
-                        </Card.Header>
+                            {users[questions[q.id].author].name} asks:
+                    </Card.Header>
                         <Card.Body>
                             <Row>
                                 <Col md={{ span: 4 }} style={{ textAlign: 'center', marginTop: 'auto', marginBottom: 'auto' }}>
-                                    <img src={users[questions[a].author].avatarURL} style={{ borderRadius: '200px', width: '85%' }} />
+                                    <img src={users[questions[q.id].author].avatarURL} style={{ borderRadius: '200px', width: '85%' }} />
                                 </Col>
                                 <Col>
                                     <Card.Title>Would you rather...</Card.Title>
 
-                                    <ListGroup.Item>...{questions[a][answers[a]].text} ...</ListGroup.Item>
-                                    
-                                    <Link to={'questions/' + questions[a].id} >
+                                    <ListGroup.Item>...{questions[q.id][answers[q.id]].text} ...</ListGroup.Item>
+
+                                    <Link to={'questions/' + q.id} >
                                         <Button className="mt-2" variant="primary">View Poll</Button>
                                     </Link>
-
                                 </Col>
                             </Row>
                         </Card.Body>
                     </Card>
                 ))
+                
             }
         </div>
     )
